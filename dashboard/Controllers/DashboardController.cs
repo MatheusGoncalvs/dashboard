0783068c9d4d0d1a5ed2c243d1838fe4d3df51ce
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2020-04-19 13:08:44 -0300

    envio production

--- dashboard/Controllers/DashboardController.cs
+++ dashboard/Controllers/DashboardController.cs
@@ -6,9 +6,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace dashboard.Controllers
 {
+    [Authorize]
     public class DashboardController : Controller
     {
         private readonly IDashboardRepository dashboardRepository;
