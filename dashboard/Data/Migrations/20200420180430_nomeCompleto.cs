285fd0b11c8d1110a038a50f72f15b47df6aba9a
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2020-04-20 16:56:21 -0300

    Implementado o front das telas para autenticação
    +Login, Registrar, Esqueci a senha

---
+++ dashboard/Data/Migrations/20200420180430_nomeCompleto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dashboard.Data.Migrations
+{
+    public partial class nomeCompleto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NomeCompleto",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NomeCompleto",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+    }
+}
